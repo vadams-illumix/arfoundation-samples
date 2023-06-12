ea403a75fa862ceb4a539bef26a23251ce7f05a3
Valarie Adams <vadams@illumix.com>
2023-06-12 12:33:29 -0700

    added XR test environments

---
+++ Assets/Scripts/Debug/FrameDrop.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FrameDropDetector : MonoBehaviour
+{
+    public float frameRateThreshold = 60.0f;
+
+    private float timeSinceLastFrame;
+
+    void Update()
+    {
+        float frameTime = Time.deltaTime;
+        float fps = 1.0f / frameTime;
+
+        if (fps < frameRateThreshold)
+        {
+            Debug.Log("Frame drop detected: " + fps + " FPS");
+        }
+
+        timeSinceLastFrame = frameTime;
+    }
+}
+
